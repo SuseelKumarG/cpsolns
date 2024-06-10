0095c5f23f99f39c1b9a6adde0fa46ff66639374
TrickyTick <suseelkumar2006@gmail.com>
2024-06-10 11:08:56 +0530

    preglobal26b

---
+++ largeadditiion.cpp
@@ -0,0 +1,84 @@
+    //in all subarrays of array a of length k have their or same then it is true for any sub array of length > k
+    //END
+    //to coprime numbers a and b can reproduce any number x greater than a*b-a-b as x= m*a+n*b
+    //END
+    #include <bits/stdc++.h>
+    using namespace std;
+    
+    typedef long long ll;
+    typedef pair<int, int> pii;
+    typedef pair<ll, ll> pll;
+    typedef pair<string, string> pss;
+    typedef vector<int> vi;
+    typedef vector<vi> vvi;
+    typedef vector<pii> vii;
+    typedef vector<ll> vl;
+    typedef vector<vl> vvl; 
+    typedef vector<bool> vb;
+    #define MAX 1000000007
+    #define N 10010
+
+    long long gcdl(long long a, long long b){
+        while(a > 0 && b > 0){
+            if(a > b){
+                a %= b;
+            }
+            else{
+                b %= a;
+            }
+        }
+        return a + b;
+    }  
+    ll lcml(ll a,ll b)
+    {
+        a=(a*b)/gcdl(a,b);
+        return a;
+    }
+    int gcd(int a, int b){
+        while(a > 0 && b > 0){
+            if(a > b){
+                a %= b;
+            }
+            else{
+                b %= a;
+            }
+        }
+        return a + b;
+    }
+    int lcml(int a,int b)
+    {
+        a=(a*b)/gcd(a,b);
+        return a;
+    }
+
+    void solve()
+    {
+        bool poss=1;
+        ll n;
+        cin>>n;
+        if(n%10==9)
+        poss=0;
+        n/=10;
+        while(n>=10)
+        {
+            if(n%10==0)
+            poss=0;
+            n/=10;
+        }
+        if(n!=1)
+        poss=0;
+        cout<<(poss?"YES":"NO")<<'\n';
+    }
+
+    int main()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        ll t=1;
+        cin>>t;
+        while(t--)
+        {
+            solve();
+        }
+        return 0;
+    }
